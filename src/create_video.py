a8fbc54fc28651ce24341a9831c1a0a9da3ea2c4
Basile Roth <basileroth.75@gmail.com>
2020-06-06 19:09:11 -0400

    Script to create a video from frames

---
+++ src/create_video.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import glob
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir("../video/View_001") if isfile(join("../video/View_001", f))]
+ 
+filename_array = []
+img_array = []
+
+for i in range(0,len(onlyfiles)):
+	number = ""
+	if i < 10:
+		number = "00"+str(i)
+	elif i<100:
+		number = "0"+str(i)
+	else :
+		number = str(i)
+	filename = "../video/View_001/frame_0"+number+".jpg"
+	filename_array.append(filename)
+ 
+
+for filename in filename_array:
+	img = cv2.imread(filename)
+	height, width, layers = img.shape
+	size = (width,height)
+	img_array.append(img)
+
+fourcc2 = cv2.VideoWriter_fourcc(*"MJPG")
+output_video_2 = cv2.VideoWriter("output_2.avi", fourcc2, 25,(width, height), True)
+
+
+for i in range(len(img_array)):
+	output_video_2.write(img_array[i])
+output_video_2.release()
