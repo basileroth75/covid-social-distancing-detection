ccedcf34dc4e653e6c2bd10b632c271cdd66fead
Basile Roth <basileroth.75@gmail.com>
2020-06-13 23:59:33 -0400

    minor changesé

--- src/bird_view_transfo.py
+++ src/bird_view_transfo.py
@@ -2,23 +2,26 @@ import numpy as np
 import cv2
 
 
-def compute_perspective_transform(corner_points,width,height):
+def compute_perspective_transform(corner_points,width,height,image):
 	""" Compute the transformation matrix
-    @ p1,p2,p3,p4 : 4 corner points 
-    @ height, width : needed parameter to determinen the matrix
+    @ corner_points : 4 corner points selected from the image
+    @ height, width : size of the image
     """
-	# Create an array out of theses points
+	# Create an array out of the 4 corner points
 	corner_points_array = np.float32(corner_points)
-	# Create an array with the 
+	# Create an array with the parameters (the dimensions) required to build the matrix
 	img_params = np.float32([[0,0],[width,0],[0,height],[width,height]])
-	# Compute the transformation matrix
-	return cv2.getPerspectiveTransform(corner_points_array,img_params) 
+	# Compute and return the transformation matrix
+	matrix = cv2.getPerspectiveTransform(corner_points_array,img_params) 
+	img_transformed = cv2.warpPerspective(image,matrix,(width,height))
+	return matrix,img_transformed
 
 
 def compute_point_perspective_transformation(matrix,list_downoids):
-	""" Apply the perspective transformation to every poins which have been detected on the main frame.
+	""" Apply the perspective transformation to every ground point which have been detected on the main frame.
     @ matrix : the 3x3 matrix 
     @ list_downoids : list that contains the points to transform
+    return : list containing all the new points
     """
 	# Compute the new coordinates of our points
 	list_points_to_detect = np.float32(list_downoids).reshape(-1, 1, 2)
